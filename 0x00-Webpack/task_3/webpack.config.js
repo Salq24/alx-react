6220fddf561b9021b35d2c6a16b268f65d674da6
Salq24 <142992679+Salq24@users.noreply.github.com>
2024-12-04 12:45:39 +0000

    webpack

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,68 +1,29 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	plugins: [
-		new HTMLWebpackPlugin({
-			filename: './index.html',
-		}),
-		new CleanWebpackPlugin(),
-		],
-	devtool: 'inline-source-map',
-	mode: 'development',
-	entry: {
-		header: {
-			import: './modules/header/header.js',
-			dependOn: 'shared',
-		},
-		body: {
-			import: './modules/body/body.js',
-			dependOn: 'shared',
-		},
-		footer: {
-			import: './modules/footer/footer.js',
-			dependOn: 'shared',
-		},
-		shared: 'jquery',
-	},
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: '[name].bundle.js',
-	},
-	optimization: {
-		splitChunks: {
-			chunks: 'all',
-		},
-	},
-	devServer: {
-		static: path.join(__dirname, './public'),
-		open: true,
-		port: 8564,
-	},
-	performance: {
-		maxAssetSize: 1000000,
-	},  
+  mode: 'development',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
   module: {
-	rules: [
-		{
-			test: /\.css$/i,
-			use: ["css-loader", "style-loader"],
-		},
-		{
-			test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
-			type: 'asset/resource',
-			use: [
-				"file-loader",
-				{
-					loader: "image-webpack-loader",
-					options: {
-							bypassingOnDebug: true,
-							disable: true,
-					},
-				},
-			],
-		},
-	],
-},
-};
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?
